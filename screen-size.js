aa0a9ad4594500193b0cd0084b528c8a50ed9627
nenthHD <133820012+nenthHD@users.noreply.github.com>
2023-05-17 00:23:09 +0200

    Delete screen-size.js

--- screen-size.js
+++
@@ -1,7 +0,0 @@
-var win = window,
-    doc = document,
-    docElem = doc.documentElement,
-    body = doc.getElementsByTagName('body')[0],
-    x = win.innerWidth || docElem.clientWidth || body.clientWidth,
-    y = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
-alert(x + ' × ' + y);
