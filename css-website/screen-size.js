cb91f63100804fdf3afb54ce11b9fd109caf791e
nenthHD <133820012+nenthHD@users.noreply.github.com>
2023-05-17 00:20:58 +0200

    Add files via upload

---
+++ css-website/screen-size.js
@@ -0,0 +1,7 @@
+var win = window,
+    doc = document,
+    docElem = doc.documentElement,
+    body = doc.getElementsByTagName('body')[0],
+    x = win.innerWidth || docElem.clientWidth || body.clientWidth,
+    y = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
+alert(x + ' × ' + y);
